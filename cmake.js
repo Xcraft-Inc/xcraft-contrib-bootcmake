c549893960b5ed126acd3462e584d3db140699fb
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-11-04 09:28:48 +0100

    Merge branch 'xcraft'

--- cmake.js
+++ cmake.js
@@ -17,6 +17,8 @@ var cmd = {};
 var makeRun = function (callback) {
   zogLog.info ('begin building of cmake');
 
+  process.env.SHELL = cmd.exe;
+
   var os = require ('os');
   var list = [
     'all',
