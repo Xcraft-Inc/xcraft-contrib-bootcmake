c82b6307f31eb6a9900f9534626fbcb6c05af08d
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-05-14 10:17:40 +0200

    Cosmetic: fix formatting

--- cmake.js
+++ cmake.js
@@ -1,35 +1,33 @@
 'use strict';
 
-var path  = require ('path');
+var path = require ('path');
 var async = require ('async');
 
-var xPlatform    = require ('xcraft-core-platform');
-var xFs          = require ('xcraft-core-fs');
-var xEnv         = require ('xcraft-core-env');
-
+var xPlatform = require ('xcraft-core-platform');
+var xFs = require ('xcraft-core-fs');
+var xEnv = require ('xcraft-core-env');
 
 var cmd = {};
 
-
 exports.getGenerator = function () {
   switch (xPlatform.getOs ()) {
-  case 'win': {
-    return 'MinGW Makefiles';
-  }
-  default: {
-    return 'Unix Makefiles';
-  }
+    case 'win': {
+      return 'MinGW Makefiles';
+    }
+    default: {
+      return 'Unix Makefiles';
+    }
   }
 };
 
 exports.getMakeTool = function () {
   switch (xPlatform.getOs ()) {
-  case 'win': {
-    return 'mingw32-make';
-  }
-  default: {
-    return 'make';
-  }
+    case 'win': {
+      return 'mingw32-make';
+    }
+    default: {
+      return 'make';
+    }
   }
 };
 
@@ -40,7 +38,6 @@ exports.stripShForMinGW = function () {
     return list;
   }
 
-
   /* Strip MSYS from the PATH. */
   while (true) {
     const sh = xEnv.var.path.isIn ('sh.exe');
@@ -49,8 +46,8 @@ exports.stripShForMinGW = function () {
     }
 
     list.push ({
-      index:    sh.index,
-      location: xEnv.var.path.strip (sh.index)
+      index: sh.index,
+      location: xEnv.var.path.strip (sh.index),
     });
   }
 
@@ -71,51 +68,54 @@ var getJobs = function (force) {
 var makeRun = function (makeDir, make, jobs, response, callback) {
   response.log.info ('begin building of cmake');
 
-  var list = [
-    'all',
-    'install'
-  ];
+  var list = ['all', 'install'];
 
   const xProcess = require ('xcraft-core-process') ({
     logger: 'xlog',
     parser: 'cmake',
-    resp:   response
+    resp: response,
   });
 
   var currentDir = process.cwd ();
   process.chdir (makeDir);
-  async.eachSeries (list, function (args, callback) {
-    var fullArgs = ['-j' + getJobs (jobs)].concat (args);
+  async.eachSeries (
+    list,
+    function (args, callback) {
+      var fullArgs = ['-j' + getJobs (jobs)].concat (args);
 
-    xProcess.spawn (make, fullArgs, {}, function (err) {
-      callback (err ? 'make failed: ' + err : null);
-    });
-  }, function (err) {
-    if (!err) {
-      response.log.info ('cmake is built and installed');
+      xProcess.spawn (make, fullArgs, {}, function (err) {
+        callback (err ? 'make failed: ' + err : null);
+      });
+    },
+    function (err) {
+      if (!err) {
+        response.log.info ('cmake is built and installed');
+      }
+
+      process.chdir (currentDir);
+      callback (err ? 'make failed' : null);
     }
-
-    process.chdir (currentDir);
-    callback (err ? 'make failed' : null);
-  });
+  );
 };
 
 /* TODO: must be generic. */
 var bootstrapRun = function (cmakeDir, response, callback) {
-  const pkgConfig = require ('xcraft-core-etc') (null, response).load ('xcraft-contrib-bootcmake');
+  const pkgConfig = require ('xcraft-core-etc') (null, response).load (
+    'xcraft-contrib-bootcmake'
+  );
 
   /* FIXME, TODO: use a backend (a module) for building cmake. */
   /* bootstrap --prefix=/mingw && make && make install */
   var args = [
     'bootstrap',
     '--parallel=' + getJobs (),
-    '--prefix=' + path.resolve (pkgConfig.out)
+    '--prefix=' + path.resolve (pkgConfig.out),
   ];
 
   const xProcess = require ('xcraft-core-process') ({
     logger: 'xlog',
     parser: 'cmake',
-    resp:   response
+    resp: response,
   });
 
   var currentDir = process.cwd ();
@@ -128,7 +128,9 @@ var bootstrapRun = function (cmakeDir, response, callback) {
 
 /* TODO: must be generic. */
 var cmakeRun = function (srcDir, response, callback) {
-  const pkgConfig = require ('xcraft-core-etc') (null, response).load ('xcraft-contrib-bootcmake');
+  const pkgConfig = require ('xcraft-core-etc') (null, response).load (
+    'xcraft-contrib-bootcmake'
+  );
 
   /* FIXME, TODO: use a backend (a module) for building with cmake. */
   /* cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr . && make all install */
@@ -140,7 +142,7 @@ var cmakeRun = function (srcDir, response, callback) {
     '-DCMAKE_COLOR_MAKEFILE=OFF',
     '-DCMAKE_BUILD_TYPE=Release',
     '-DCMAKE_INSTALL_PREFIX:PATH=' + path.resolve (pkgConfig.out),
-    srcDir
+    srcDir,
   ];
 
   args.unshift ('-G', exports.getGenerator ());
@@ -148,7 +150,7 @@ var cmakeRun = function (srcDir, response, callback) {
   const xProcess = require ('xcraft-core-process') ({
     logger: 'xlog',
     parser: 'cmake',
-    resp:   response
+    resp: response,
   });
 
   var currentDir = process.cwd ();
@@ -161,7 +163,7 @@ var cmakeRun = function (srcDir, response, callback) {
 
 var patchRun = function (srcDir, response, callback) {
   var xDevel = require ('xcraft-core-devel');
-  var async  = require ('async');
+  var async = require ('async');
 
   var os = xPlatform.getOs ();
 
@@ -173,119 +175,168 @@ var patchRun = function (srcDir, response, callback) {
     return;
   }
 
-  async.eachSeries (list, function (file, callback) {
-    response.log.info ('apply patch: ' + file);
-    var patchFile = path.join (patchDir, file);
+  async.eachSeries (
+    list,
+    function (file, callback) {
+      response.log.info ('apply patch: ' + file);
+      var patchFile = path.join (patchDir, file);
 
-    xDevel.patch (srcDir, patchFile, 1, response, function (err) {
-      callback (err ? 'patch failed: ' + file + ' ' + err : null);
-    });
-  }, function (err) {
-    callback (err);
-  });
+      xDevel.patch (srcDir, patchFile, 1, response, function (err) {
+        callback (err ? 'patch failed: ' + file + ' ' + err : null);
+      });
+    },
+    function (err) {
+      callback (err);
+    }
+  );
 };
 
 /**
  * Build the cmake package.
  */
 cmd.build = function (msg, response) {
-  const xcraftConfig = require ('xcraft-core-etc') (null, response).load ('xcraft');
-  const pkgConfig    = require ('xcraft-core-etc') (null, response).load ('xcraft-contrib-bootcmake');
+  const xcraftConfig = require ('xcraft-core-etc') (null, response).load (
+    'xcraft'
+  );
+  const pkgConfig = require ('xcraft-core-etc') (null, response).load (
+    'xcraft-contrib-bootcmake'
+  );
 
   var archive = path.basename (pkgConfig.src);
-  var inputFile  = pkgConfig.src;
+  var inputFile = pkgConfig.src;
   var outputFile = path.join (xcraftConfig.tempRoot, 'src', archive);
 
-  async.auto ({
-    taskHttp: function (callback) {
-      var xHttp = require ('xcraft-core-http');
+  async.auto (
+    {
+      taskHttp: function (callback) {
+        var xHttp = require ('xcraft-core-http');
 
-      xHttp.get (inputFile, outputFile, function () {
-        callback ();
-      }, function (progress, total) {
-        response.log.progress ('Downloading', progress, total);
-      });
+        xHttp.get (
+          inputFile,
+          outputFile,
+          function () {
+            callback ();
+          },
+          function (progress, total) {
+            response.log.progress ('Downloading', progress, total);
+          }
+        );
+      },
+
+      taskExtract: [
+        'taskHttp',
+        function (callback) {
+          var xExtract = require ('xcraft-core-extract');
+          var outDir = path.dirname (outputFile);
+
+          xExtract.targz (
+            outputFile,
+            outDir,
+            null,
+            response,
+            function (err) {
+              callback (
+                err ? 'extract failed: ' + err : null,
+                path.join (outDir, path.basename (outputFile, '.tar.gz'))
+              );
+            },
+            function (progress, total) {
+              response.log.progress ('Extracting', progress, total);
+            }
+          );
+        },
+      ],
+
+      taskPatch: [
+        'taskExtract',
+        function (callback, results) {
+          patchRun (results.taskExtract, response, callback);
+        },
+      ],
+
+      taskPrepare: [
+        'taskPatch',
+        function (callback) {
+          var cmake = xEnv.var.path.isIn ('cmake' + xPlatform.getExecExt ());
+          callback (null, cmake);
+        },
+      ],
+
+      taskBootstrap: [
+        'taskPrepare',
+        function (callback, results) {
+          if (!results.taskPrepare) {
+            bootstrapRun (results.taskExtract, response, callback);
+          } else {
+            callback ();
+          }
+        },
+      ],
+
+      taskMSYS: [
+        'taskPrepare',
+        function (callback, results) {
+          var res = {
+            cmake: null,
+            path: null,
+          };
+
+          if (!results.taskPrepare) {
+            res.cmake = false;
+            callback (null, res);
+            return;
+          }
+
+          res.cmake = true;
+          res.path = exports.stripShForMinGW ();
+
+          callback (null, res);
+        },
+      ],
+
+      taskCMake: [
+        'taskMSYS',
+        function (callback, results) {
+          if (results.taskMSYS.cmake) {
+            cmakeRun (results.taskExtract, response, callback);
+          } else {
+            callback ();
+          }
+        },
+      ],
+
+      taskMake: [
+        'taskBootstrap',
+        'taskCMake',
+        function (callback, results) {
+          var buildDir = results.taskMSYS.cmake
+            ? path.join (results.taskExtract, '../BUILD_CMAKE')
+            : results.taskExtract;
+          makeRun (
+            buildDir,
+            results.taskMSYS.cmake ? exports.getMakeTool () : 'make',
+            results.taskMSYS.cmake,
+            response,
+            callback
+          );
+        },
+      ],
     },
-
-    taskExtract: ['taskHttp', function (callback) {
-      var xExtract = require ('xcraft-core-extract');
-      var outDir = path.dirname (outputFile);
-
-      xExtract.targz (outputFile, outDir, null, response, function (err) {
-        callback (err ? 'extract failed: ' + err : null,
-                  path.join (outDir, path.basename (outputFile, '.tar.gz')));
-      }, function (progress, total) {
-        response.log.progress ('Extracting', progress, total);
-      });
-    }],
-
-    taskPatch: ['taskExtract', function (callback, results) {
-      patchRun (results.taskExtract, response, callback);
-    }],
-
-    taskPrepare: ['taskPatch', function (callback) {
-      var cmake = xEnv.var.path.isIn ('cmake' + xPlatform.getExecExt ());
-      callback (null, cmake);
-    }],
-
-    taskBootstrap: ['taskPrepare', function (callback, results) {
-      if (!results.taskPrepare) {
-        bootstrapRun (results.taskExtract, response, callback);
-      } else {
-        callback ();
-      }
-    }],
-
-    taskMSYS: ['taskPrepare', function (callback, results) {
-      var res = {
-        cmake: null,
-        path: null
-      };
-
-      if (!results.taskPrepare) {
-        res.cmake = false;
-        callback (null, res);
-        return;
+    function (err, results) {
+      if (err) {
+        response.log.err (err);
       }
 
-      res.cmake = true;
-      res.path = exports.stripShForMinGW ();
-
-      callback (null, res);
-    }],
-
-    taskCMake: ['taskMSYS', function (callback, results) {
-      if (results.taskMSYS.cmake) {
-        cmakeRun (results.taskExtract, response, callback);
-      } else {
-        callback ();
+      /* Restore MSYS path. */
+      if (results.taskMSYS && results.taskMSYS.path) {
+        for (const p of results.taskMSYS.path) {
+          xEnv.var.path.insert (p.index, p.location);
+        }
       }
-    }],
 
-    taskMake: ['taskBootstrap', 'taskCMake', function (callback, results) {
-      var buildDir = results.taskMSYS.cmake ?
-                     path.join (results.taskExtract, '../BUILD_CMAKE') :
-                     results.taskExtract;
-      makeRun (buildDir,
-               results.taskMSYS.cmake ? exports.getMakeTool () : 'make',
-               results.taskMSYS.cmake,
-               response,
-               callback);
-    }]
-  }, function (err, results) {
-    if (err) {
-      response.log.err (err);
+      response.events.send ('cmake.build.finished');
     }
-
-    /* Restore MSYS path. */
-    if (results.taskMSYS && results.taskMSYS.path) {
-      for (const p of results.taskMSYS.path) {
-        xEnv.var.path.insert (p.index, p.location);
-      }
-    }
-
-    response.events.send ('cmake.build.finished');
-  });
+  );
 };
 
 /**
@@ -297,6 +348,6 @@ exports.xcraftCommands = function () {
   const xUtils = require ('xcraft-core-utils');
   return {
     handlers: cmd,
-    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json'))
+    rc: xUtils.json.fromFile (path.join (__dirname, './rc.json')),
   };
 };
--- config.js
+++ config.js
@@ -3,24 +3,29 @@
 /**
  * Retrieve the inquirer definition for xcraft-core-etc.
  */
-module.exports = [{
-  type: 'input',
-  name: 'name',
-  message: 'package name',
-  default: 'cmake'
-}, {
-  type: 'input',
-  name: 'version',
-  message: 'version',
-  default: '3.5.2'
-}, {
-  type: 'input',
-  name: 'src',
-  message: 'source URI',
-  default: 'http://www.cmake.org/files/v3.5/cmake-3.5.2.tar.gz'
-}, {
-  type: 'input',
-  name: 'out',
-  message: 'output directory',
-  default: './usr'
-}];
+module.exports = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'package name',
+    default: 'cmake',
+  },
+  {
+    type: 'input',
+    name: 'version',
+    message: 'version',
+    default: '3.5.2',
+  },
+  {
+    type: 'input',
+    name: 'src',
+    message: 'source URI',
+    default: 'http://www.cmake.org/files/v3.5/cmake-3.5.2.tar.gz',
+  },
+  {
+    type: 'input',
+    name: 'out',
+    message: 'output directory',
+    default: './usr',
+  },
+];
